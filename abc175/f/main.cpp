8a7aa9e42a73ad002005b9e0d0eaab1d893ed613
slimepiki <pikisli@icloud.com>
2024-07-21 21:49:55 +0900

    directory arrangement

---
+++ abc175/f/main.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+using ll=long long;
+using ull=unsigned long long;
+
+#define _overload3(_1,_2,_3,name,...) name
+#define _rep(i,n) repi(i,0,n)
+#define repi(i,a,b) for(int i=int(a);i<int(b);++i)
+#define rep(...) _overload3(__VA_ARGS__,repi,_rep,)(__VA_ARGS__)
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip> 
+#include <string>
+#include<algorithm>
+
+
+int main(){
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+
+
+    return 0;
+}
