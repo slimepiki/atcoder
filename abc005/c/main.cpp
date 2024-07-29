d4c1f407420bd66ec6cc53df634c9e39e3f1c19b
slimepiki <pikisli@icloud.com>
2024-07-29 21:59:24 +0900

    ざこ問題だったが自分のほうがよりざこだった(abc005/c)

---
+++ abc005/c/main.cpp
@@ -0,0 +1,101 @@
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef __LOCAL
+#define debug(...)                                                      \
+    cerr << "\033[33m(line:" << __LINE__ << ") " << "[" << #__VA_ARGS__ \
+         << "]:",                                                       \
+        debug_out(__VA_ARGS__);                                         \
+    cerr << "\033[m";
+#else
+#define debug(...) ;
+#endif
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+
+#define ii tuple<int, int>
+#define iiget(t, x, y) \
+    x = get<0>(t);     \
+    y = get<1>(t);
+#define iii tuple<int, int, int>
+#define iiiget(t, x, y, z) \
+    x = get<0>(t);         \
+    y = get<1>(t);         \
+    z = get<2>(t);
+#define vi vector<int>
+#define initsvi(a, S, N) static vi a(S, N);
+#define vvi vector<vi>
+#define initsvvi(a, H, W, N) static vvi a(H, vi(W, N));
+#define vll vector<ll>
+#define initsvll(a, S, N) static vll a(S, N);
+
+#define vc vector<char>
+#define initsvc(a, S, N) static vc a(S, N);
+#define vvc vector<vc>
+#define initsvvc(a, H, W, N) static vvc a(H, vc(W, N));
+
+template <typename T>
+inline bool chmin(T& a, const T& b) {
+    bool compare = a > b;
+    if (a > b) a = b;
+    return compare;
+}
+template <typename T>
+inline bool chmax(T& a, const T& b) {
+    bool compare = a < b;
+    if (a < b) a = b;
+    return compare;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    vi tkyk;
+    static int T, N, M,t,kt,sell;
+    cin >> T >> N;
+    static bool ans;
+    rep(i, N){
+        cin >> t;
+        tkyk.push_back(t);
+    }
+    cin >> M;
+    ans = true;
+
+    rep(i, M){
+        cin >> kt;
+        t = -1;
+        rep(j, N){
+            if(tkyk[j] <= kt && (kt - tkyk[j]) <= T && (kt - tkyk[j]) > t){
+                t = kt - tkyk[j];
+                sell = j;
+            }
+            
+        }
+            if(t == -1){
+                ans = false;
+                break;
+            }else tkyk[sell] = 10000000;
+    }
+
+
+    if(ans)cout << "yes" << endl;
+    else cout << "no" << endl;
+    return 0;
+}
