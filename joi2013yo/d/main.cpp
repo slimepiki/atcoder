17d198d2b8e3b55f214e7002a40edaa3392ebe87
slimepiki <pikisli@icloud.com>
2024-08-06 18:55:15 +0900

    完全に治ったね(バグが)(joi2013/d)

--- joi2013yo/d/main.cpp
+++ joi2013yo/d/main.cpp
@@ -18,7 +18,7 @@ void debug_out(Head H, Tail... T) {
         debug_out(__VA_ARGS__);                                         \
     cerr << "\033[m";
 #else
-#define debug(...) //   :)
+#define debug(...)  //   :)
 #endif
 #define _overload3(_1, _2, _3, name, ...) name
 #define _rep(i, n) repi(i, 0, n)
@@ -37,16 +37,16 @@ void debug_out(Head H, Tail... T) {
 #define vi vector<int>
 #define initvi(a, S, N) vi a(S, N);
 #define vvi vector<vi>
-#define initvvi(a, H, W, N)  vvi a(H, vi(W, N));
+#define initvvi(a, H, W, N) vvi a(H, vi(W, N));
 #define vll vector<ll>
-#define initvll(a, S, N)  vll a(S, N);
+#define initvll(a, S, N) vll a(S, N);
 #define vvll vector<vll>
 #define initvvll(a, H, W, N) vvll a(H, vll(W, N));
 
 #define vc vector<char>
-#define initvc(a, S, N)  vc a(S, N);
+#define initvc(a, S, N) vc a(S, N);
 #define vvc vector<vc>
-#define initvvc(a, H, W, N)  vvc a(H, vc(W, N));
+#define initvvc(a, H, W, N) vvc a(H, vc(W, N));
 
 template <typename T>
 inline bool chmin(T& a, const T& b) {
@@ -65,5 +65,41 @@ int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
+    static int D, N, max, r, pr;
+    cin >> D >> N;
+
+    vector<int> d(D, 0);
+    rep(i, D) cin >> d[i];
+    vvi c(N, vi(3, 0));
+    rep(j, N) cin >> c[j][0] >> c[j][1] >> c[j][2];
+
+    int dp[2][N]{};
+
+    rep(i, D) {
+        debug(i)
+        r = i % 2;
+        pr = (i + 1) % 2;
+
+        memset(dp[pr], 0, N * sizeof(int));
+        rep(j, N) {
+
+            if (c[j][0] <= d[i] && c[j][1] >= d[i]) {
+                if (i == 0)
+                    dp[pr][j] = 0;
+                else
+                    rep(k, N){
+                        chmax(dp[pr][j], dp[r][k] + abs(c[k][2] - c[j][2]));
+                        debug(dp[pr][j]);
+
+                    }
+            } else {
+                dp[pr][j] = -1000;
+            }
+            //debug(dp[pr][j]);
+        }
+    }
+    max = 0;
+    rep(i, N) chmax(max, dp[D % 2][i]);
+    cout << max << endl;
     return 0;
 }
--- joi2013yo/d/main_old.cpp
+++ joi2013yo/d/main_old.cpp
@@ -4,11 +4,6 @@ using ull = unsigned long long;
 
 #include <bits/stdc++.h>
 
-#include <algorithm>
-#include <iomanip>
-#include <iostream>
-#include <string>
-
 void debug_out() { cerr << endl; }
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) {
@@ -23,7 +18,7 @@ void debug_out(Head H, Tail... T) {
         debug_out(__VA_ARGS__);                                         \
     cerr << "\033[m";
 #else
-#define debug(...) ;
+#define debug(...)  //   :)
 #endif
 #define _overload3(_1, _2, _3, name, ...) name
 #define _rep(i, n) repi(i, 0, n)
@@ -40,16 +35,18 @@ void debug_out(Head H, Tail... T) {
     y = get<1>(t);         \
     z = get<2>(t);
 #define vi vector<int>
-#define initsvi(a, S, N) static vi a(S, N);
+#define initvi(a, S, N) vi a(S, N);
 #define vvi vector<vi>
-#define initsvvi(a, H, W, N) static vvi a(H, vi(W, N));
+#define initvvi(a, H, W, N) vvi a(H, vi(W, N));
 #define vll vector<ll>
-#define initsvll(a, S, N) static vll a(S, N);
+#define initvll(a, S, N) vll a(S, N);
+#define vvll vector<vll>
+#define initvvll(a, H, W, N) vvll a(H, vll(W, N));
 
 #define vc vector<char>
-#define initsvc(a, S, N) static vc a(S, N);
+#define initvc(a, S, N) vc a(S, N);
 #define vvc vector<vc>
-#define initsvvc(a, H, W, N) static vvc a(H, vc(W, N));
+#define initvvc(a, H, W, N) vvc a(H, vc(W, N));
 
 template <typename T>
 inline bool chmin(T& a, const T& b) {
@@ -68,12 +65,48 @@ int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    int a,b,c;
+    static int D, N, maxHade, minHade, a0, a1;
+    cin >> D >> N;
 
-    cin >> a >> b >> c;
+    static vector<int> d(D, 0);
+    rep(i, D) cin >> d[i];
+    static vvi c(N, vi(3, 0));
+    rep(j, N) 
+        cin >> c[j][0] >> c[j][1] >> c[j][2];
 
-    if(a + b >= c)cout << "Yes" << endl;
-    else cout << "No" << endl;
+    sort(c.begin(), c.end(), [](vector<int> const& a, vector<int> const& b) {
+        return a[2] < b[2];
+    });  // 派手さ昇順
+
+    //[0][-] : 始めできるだけ地味,[1][-] :逆
+    //[-][0] : トータルスコア, [-][1] :その日の派手さ
+    int dp[2][2];
+    memset(dp, 0, 4 * sizeof (int));
+
+    rep(i, D) {
+        for(int j = 0; j < N;j++){
+            if(d[i]>=c[j][0] && d[i] <=c[j][1]){
+                minHade = c[j][2];
+                break;
+            }
+        }
+        for(int j = N - 1; j >= 0;j--){
+            if(d[i]>=c[j][0] && d[i] <=c[j][1]){
+                maxHade = c[j][2];
+                break;
+            }
+        }
+
+        a0 = (i % 2) ? minHade : maxHade;
+        a1 = (i % 2) ? maxHade : minHade;
+
+        if(i > 0)dp[0][0] += abs(dp[0][1] - a0);
+        dp[0][1] = a0;
+        if(i > 0)dp[1][0] += abs(dp[0][1] - a1);
+        dp[1][1] = a1;
+    }
+
+    cout << max(dp[0][0], dp[1][0]) << endl;
 
     return 0;
 }
