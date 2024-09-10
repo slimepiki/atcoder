a42098e0e92aa1ecb2ac4faeb6a04913e51d6078
slimepiki <pikisli@icloud.com>
2024-09-10 19:57:44 +0900

    2.5秒で解ける

---
+++ abc108/b/main.cpp
@@ -0,0 +1,81 @@
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+#include <bits/stdc++.h>
+
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef __LOCAL
+#define debug(...)                                                      \
+    cerr << "\033[33m(line:" << __LINE__ << ") " << "[" << #__VA_ARGS__ \
+         << "]:",                                                       \
+        debug_out(__VA_ARGS__);                                         \
+    cerr << "\033[m";
+#else
+#define debug(...)  //   :)
+#endif
+#define _overload3(_1, _2, _3, name, ...) name
+#define _rep(i, n) repi(i, 0, n)
+#define repi(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define rrep(i, a, b) for (int i = int(a); i >= int(b); --i)
+#define rep(...) _overload3(__VA_ARGS__, repi, _rep, )(__VA_ARGS__)
+
+#define ii tuple<int, int>
+#define iiget(t, x, y) \
+    x = get<0>(t);     \
+    y = get<1>(t);
+#define iii tuple<int, int, int>
+#define iiiget(t, x, y, z) \
+    x = get<0>(t);         \
+    y = get<1>(t);         \
+    z = get<2>(t);
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvvi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vvvll vector<vvll>
+
+#define vc vector<char>
+#define vvc vector<vc>
+#define vvvc vector<vvc>
+
+#define IINF 0x3f3f3f3f - 10
+
+template <typename T>
+inline bool chmin(T& a, const T& b) {
+    bool compare = a > b;
+    if (a > b) a = b;
+    return compare;
+}
+template <typename T>
+inline bool chmax(T& a, const T& b) {
+    bool compare = a < b;
+    if (a < b) a = b;
+    return compare;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int x1, y1, x2, y2;
+
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    int x3, y3, x4, y4;
+    int a = x2 - x1, b = y2 - y1;
+    x3 = x2 - b;
+    y3 = y2 + a;
+    x4 = x3 - a;
+    y4 = y3 - b;
+    cout << x3 << ' ' << y3 << ' ' << x4 << ' ' << y4 << endl;
+
+    return 0;
+}
