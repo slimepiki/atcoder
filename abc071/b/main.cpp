4b7ac94dac84ce7dee174499e1bfcdd7278537e8
slimepiki <pikisli@icloud.com>
2024-09-16 18:12:31 +0900

    streak稼ぎのbootcamp/easy

--- abc071/b/main.cpp
+++ abc071/b/main.cpp
@@ -61,79 +61,23 @@ inline bool chmax(T& a, const T& b) {
     return compare;
 }
 
-
-// modつきint
-constexpr int mod = 1000000007;
-class mint {
-   public:
-    long long x;
-    constexpr mint(long long x = 0) : x((x % mod + mod) % mod) {}
-    constexpr mint operator-() const { return mint(-x); }
-    constexpr mint& operator+=(const mint& a) {
-        if ((x += a.x) >= mod) x -= mod;
-        return *this;
-    }
-    constexpr mint& operator-=(const mint& a) {
-        if ((x += mod - a.x) >= mod) x -= mod;
-        return *this;
-    }
-    constexpr mint& operator*=(const mint& a) {
-        (x *= a.x) %= mod;
-        return *this;
-    }
-    constexpr mint operator+(const mint& a) const {
-        mint res(*this);
-        return res += a;
-    }
-    constexpr mint operator-(const mint& a) const {
-        mint res(*this);
-        return res -= a;
-    }
-    constexpr mint operator*(const mint& a) const {
-        mint res(*this);
-        return res *= a;
-    }
-    constexpr mint pow(long long t) const {
-        if (!t) return 1;
-        mint a = pow(t >> 1);
-        a *= a;
-        if (t & 1) a *= *this;
-        return a;
-    }
-    constexpr operator ll() const{
-        return x;
-    }
-
-    // for prime mod
-    constexpr mint inv() const { return pow(mod - 2); }
-    constexpr mint& operator/=(const mint& a) { return (*this) *= a.inv(); }
-    constexpr mint operator/(const mint& a) const {
-        mint res(*this);
-        return res /= a;
-    }
-};
-ostream& operator<<(ostream& os, const mint& m) {
-    os << m.x;
-    return os;
-}
-
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
+    
+    char c;
+    bool flags[26]{};
 
-    int N;
-    cin >> N;
+    while(cin >> c){
+        flags[c- 'a'] = true;
+    }
 
-    vector<vector<int>> bri(N, vector<int>(0,0));
-    int unsolvedcount[N];
-    int parent[N];
-
-
-
-    mint dp[N];
-
-
-
-    queue<int> q;
+    rep(i, 26){
+        if(!flags[i]){
+            cout << (char) ('a' + i) << endl;
+            return 0;
+        }
+    }
+    cout << "None" << endl;
     return 0;
 }
